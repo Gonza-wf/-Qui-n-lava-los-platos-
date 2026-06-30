4c50eb99dc9e61240ad4339727b273167ff50c22
Geezxy <gonzayobanni@gmail.com>
2026-06-29 21:38:24 -0300

    Feat: Add automatic PWA background update mechanism

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AppStateProvider } from './context/AppStateContext.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+// Automatically check for PWA updates and apply them instantly
+const updateSW = registerSW({
+  onNeedRefresh() {
+    updateSW(true);
+  },
+  onOfflineReady() {}
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
